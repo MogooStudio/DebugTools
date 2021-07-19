1e61edfb8975de65950cd8d7d4f84b46a7a339e1
jinshan <jinshan@hummergames.com>
2021-07-19 17:40:34 +0800

    Update log.py

--- log.py
+++ log.py
@@ -2,10 +2,10 @@ import datetime
 
 
 def info(msg):
-    msg = "[{0}]info: {1}".format(datetime.datetime.now(), msg)
+    msg = "[{0}][info]: {1}".format(datetime.datetime.now(), msg)
     print(msg)
 
 
 def error(msg):
-    msg = "[{0}]error: {1}".format(datetime.datetime.now(), msg)
+    msg = "[{0}][error]: {1}".format(datetime.datetime.now(), msg)
     print(msg)
