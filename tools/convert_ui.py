2112e37858267322df82e9fda5b9cfbeeef10919
jinshan <jinshan@hummergames.com>
2021-09-22 20:55:11 +0800

    优化ui转换脚本

--- tools/convert_ui.py
+++ tools/convert_ui.py
@@ -1,6 +1,13 @@
 import os
 import sys
+import subprocess
 
+ENV = {
+    "win32": "E:/Program Files/Python38/python3.exe",
+    "darwin": "/usr/local/Cellar/python@3.9/3.9.2_1/bin/python3.9"
+}
+
+print("Start.")
 project_path = os.path.join(sys.path[0], "..")
 for ui in os.listdir(project_path):
     if ui.endswith(".ui"):
@@ -8,6 +15,7 @@ for ui in os.listdir(project_path):
         py = ui.replace(".ui", ".py")
         py_path = os.path.join(project_path, py)
         print("ui={0} name={1}".format(ui, py))
-        cmd = "/usr/local/Cellar/python@3.9/3.9.2_1/bin/python3.9 -m PyQt5.uic.pyuic {0} -o {1}".format(ui_path, py_path)
-        os.system(cmd)
-print("ui全部转换完毕")
+        cmd = "{0} -m PyQt5.uic.pyuic {1} -o {2}".format(ENV[sys.platform], ui_path, py_path)
+        process = subprocess.Popen(cmd)
+        process.wait()
+print("Done.")
